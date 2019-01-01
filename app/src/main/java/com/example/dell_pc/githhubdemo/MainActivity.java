9e5ac8a9170254c3095bc4941a1ecb2788ae9154
Keshav <kkatad123@gmail.com>
2019-01-01 22:06:08 +0530

    test

---
+++ app/src/main/java/com/example/dell_pc/githhubdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dell_pc.githhubdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void init(){
+
+        String value = "This is for testing purpose";
+    }
+
+    public void getData(){
+
+
+    }
+}
